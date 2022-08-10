8ac4b340b7e1bdb4c4d15e903785ad3df936e0a1
= <=>
2022-08-10 22:37:05 +0400

    This app is base of all redux toolkit projects..

---
+++ src/features/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+        {/* nav */}
+        {/* routers */}
+        {/* footer */}
+    </div>
+  )
+}
+
+export default Dashboard
